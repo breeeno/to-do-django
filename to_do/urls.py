37d2f55e76ccacd6c173fd3ddec8a843c0b19ebd
Breno <bx.sptf@gmail.com>
2022-02-21 18:40:20 -0300

    - Implementando home e teste de home

--- to_do/urls.py
+++ to_do/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from to_do.home_view import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+
 ]
