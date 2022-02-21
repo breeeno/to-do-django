8a205ef135adf86bd5674902ebae4949d154e9f0
Breno <bx.sptf@gmail.com>
2022-02-21 10:51:40 -0300

    - Consertado bug de allowed_host

--- to_do/settings.py
+++ to_do/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=c%(vl-0m(r#)*j88-vn#i63_#n+$untyl*q@c2upell230ll$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
